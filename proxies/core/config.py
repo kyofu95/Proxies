3d6ce579f32f13dbcbf63d38135db4ab3b6e838e
sny <127958265+kyofu95@users.noreply.github.com>
2023-05-29 19:15:10 +0300

    base settings and database

---
+++ proxies/core/config.py
@@ -0,0 +1,10 @@
+import os
+
+
+class Settings:
+    """This class represents the settings for the application."""
+
+    DATABASE_URI: str = os.environ.get("DATABASE_URI", "")
+
+
+settings = Settings()
