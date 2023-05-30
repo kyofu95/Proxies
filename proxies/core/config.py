f7898b432b595db23ec64cf5aa5c3299e6b0ee6f
sny <127958265+kyofu95@users.noreply.github.com>
2023-05-30 18:04:32 +0300

    secret key

--- proxies/core/config.py
+++ proxies/core/config.py
@@ -5,6 +5,7 @@ class Settings:
     """This class represents the settings for the application."""
 
     DATABASE_URI: str = os.environ.get("DATABASE_URI", "")
+    SECRET_KEY: str = os.environ.get("DATABASE_URI", "")
 
 
 settings = Settings()
--- proxies/main.py
+++ proxies/main.py
@@ -1,14 +1,19 @@
-from flask import Flask
+from flask import Flask, Blueprint, render_template, flash
 
 from proxies.core.database import init_database
 from proxies.core.config import settings
+
+from proxies.views import bp as index_bp
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
     app.config["SQLALCHEMY_DATABASE_URI"] = settings.DATABASE_URI
-    print(app.config["SQLALCHEMY_DATABASE_URI"])
+    app.config["SECRET_KEY"] = settings.SECRET_KEY
 
     init_database(app)
 
+    app.register_blueprint(index_bp)
+
     return app
+
