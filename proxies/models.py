1604b808b6af14eca9aacf14af856cbf7bb79a5c
sny <127958265+kyofu95@users.noreply.github.com>
2023-05-31 15:26:02 +0300

    rework models, +migration

--- proxies/models.py
+++ proxies/models.py
@@ -6,7 +6,7 @@ from sqlalchemy.dialects.postgresql import INET
 from proxies.core.database import db
 
 
-class ProxyProtocol(enum.Enum):
+class SQLProxyProtocol(enum.Enum):
     """
     Represents different proxy protocols.
 
@@ -18,22 +18,27 @@ class ProxyProtocol(enum.Enum):
     - HTTPS: HTTPS proxy
     """
 
-    UNKNOWN = 1
-    SOCKS4 = 2
-    SOCKS5 = 3
-    HTTP = 4
-    HTTPS = 5
+    UNKNOWN = 0
+    SOCKS4 = 1
+    SOCKS5 = 2
+    HTTP = 3
+    HTTPS = 4
 
 
 class Proxy(db.Model):
     """A database model representing a proxy server."""
 
     __tablename__ = "proxy"
+    __table_args__ = (db.UniqueConstraint("ip_address", "ip_port", "protocol"),)
 
     id = db.Column(db.Integer, primary_key=True)
     ip_address = db.Column(INET, nullable=False)
     ip_port = db.Column(db.Integer, nullable=False)
-    protocol = db.Column(db.Enum(ProxyProtocol, values_callable=lambda obj: [e.name for e in obj]))
+    protocol = db.Column(
+        db.Enum(SQLProxyProtocol, values_callable=lambda obj: [e.name for e in obj]),
+        default=SQLProxyProtocol.UNKNOWN,
+        nullable=False,
+    )
     login = db.Column(db.String(100))
     password = db.Column(db.String(100))
 
@@ -42,8 +47,11 @@ class Proxy(db.Model):
 
     added = db.Column(db.DateTime, default=datetime.utcnow)
 
-    address = db.relationship("address", backref="proxy_id")
-    health = db.relationship("health", backref="proxy_id")
+    address_id = db.Column(db.ForeignKey("address.id"))
+    health_id = db.Column(db.ForeignKey("health.id"))
+
+    address = db.relationship("Address", uselist=False)
+    health = db.relationship("Health", uselist=False)
 
 
 class Address(db.Model):
@@ -56,8 +64,6 @@ class Address(db.Model):
     region = db.Column(db.String(100))
     city = db.Column(db.String(100))
 
-    proxy_id = db.Column(db.ForeignKey("proxy.id"))
-
 
 class Health(db.Model):
     """A class representing the health status of a proxy."""
@@ -69,5 +75,3 @@ class Health(db.Model):
     failed_connections = db.Column(db.Integer, default=0)
 
     last_tested = db.Column(db.DateTime, default=datetime.utcnow)
-
-    proxy_id = db.Column(db.ForeignKey("proxy.id"))
