d9f77b63761101ec2272f3e790495ca7ec2d6246
sny <127958265+kyofu95@users.noreply.github.com>
2023-06-05 16:28:31 +0300

    html: dynamically update table by country or protocol

--- proxies/views.py
+++ proxies/views.py
@@ -1,9 +1,10 @@
 from datetime import datetime, timedelta
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify
 
-from proxies.models import Proxy as DB_Proxy, Health as DB_Health
+from proxies.models import Proxy as DB_Proxy, Health as DB_Health, Address as DB_Address
 from proxies.core.database import db
+from proxies.service.proxy import ProxyProtocol
 
 bp = Blueprint("index", __name__, "templates/")
 
@@ -15,7 +16,7 @@ def td_format(td_object: timedelta) -> str:
     """
 
     seconds = int(td_object.total_seconds())
-    periods = [
+    PERIODS = [
         ("year", 60 * 60 * 24 * 365),
         ("month", 60 * 60 * 24 * 30),
         ("day", 60 * 60 * 24),
@@ -25,7 +26,7 @@ def td_format(td_object: timedelta) -> str:
     ]
 
     strings = []
-    for period_name, period_seconds in periods:
+    for period_name, period_seconds in PERIODS:
         if seconds >= period_seconds:
             period_value, seconds = divmod(seconds, period_seconds)
             has_s = "s" if period_value > 1 else ""
@@ -34,15 +35,21 @@ def td_format(td_object: timedelta) -> str:
     return ", ".join(strings)
 
 
-@bp.route("/", methods=["GET"])
-def index():
-    """Renders the index.html template with a list of proxy servers."""
+@bp.route("/filtered_data", methods=["POST"])
+def filtered_data():
+    country = request.form.get("country")
+    protocol = request.form.get("protocol")
 
-    db_proxies = (
-        db.session.execute(db.select(DB_Proxy).join(DB_Proxy.health).order_by(DB_Health.last_tested.desc()).limit(20))
-        .scalars()
-        .all()
-    )
+    statement = db.select(DB_Proxy).join(DB_Proxy.address).join(DB_Proxy.health)
+    if country:
+        statement = statement.filter(DB_Address.country == country)
+    if protocol:
+        protocol = ProxyProtocol(int(protocol))
+        statement = statement.filter(DB_Proxy.protocol == protocol)
+
+    statement = statement.order_by(DB_Health.last_tested.desc()).limit(50)
+
+    db_proxies = db.session.execute(statement).scalars().all()
 
     items = []
     for proxy in db_proxies:
@@ -50,10 +57,46 @@ def index():
             "address": str(proxy.ip_address),
             "port": proxy.ip_port,
             "country": proxy.address.country,
-            "protocol": proxy.protocol.name.lower(),
+            "protocol": proxy.protocol.name,
             "response": proxy.latency,
             "last_update": td_format(datetime.utcnow() - proxy.health.last_tested),
         }
         items.append(proxy_dict)
 
-    return render_template("index.html", items=items)
+    return jsonify(items)
+
+
+@bp.route("/", methods=["GET", "POST"])
+def index():
+    """Renders the index.html template with a list of proxy servers."""
+
+    db_proxies = (
+        db.session.execute(db.select(DB_Proxy).join(DB_Proxy.health).order_by(DB_Health.last_tested.desc()).limit(50))
+        .scalars()
+        .all()
+    )
+
+    db_coutries = (
+        db.session.execute(db.select(DB_Address.country).distinct().order_by(DB_Address.country.asc()))
+        .columns("country")
+        .all()
+    )
+
+    countries = []
+    for country in db_coutries:
+        country_name = country[0]
+        countries.append(country_name)
+
+    items = []
+    for proxy in db_proxies:
+        proxy_dict = {
+            "address": str(proxy.ip_address),
+            "port": proxy.ip_port,
+            "country": proxy.address.country,
+            "protocol": proxy.protocol.name,
+            "response": proxy.latency,
+            "last_update": td_format(datetime.utcnow() - proxy.health.last_tested),
+        }
+        items.append(proxy_dict)
+
+    return render_template("index.html", countries=countries, items=items)
