6d2dd179cfad832982b8ef82938085a2f9b99fe6
sny <127958265+kyofu95@users.noreply.github.com>
2023-06-07 18:16:59 +0300

    html: fix select x2

--- tests/test_timeformat.py
+++ tests/test_timeformat.py
@@ -1,60 +1,59 @@
 import pytest
-from unittest.mock import MagicMock
 
-from proxies.views import td_format
-from datetime import datetime, timedelta
-
-
-def test_td_format_years():
-    assert td_format(timedelta(days=365 * 3)) == "3 years"
+from proxies.views import timedelta_format
+from datetime import timedelta
 
 
-def test_td_format_months():
-    assert td_format(timedelta(days=30 * 6)) == "6 months"
+def test_timedelta_format_years():
+    assert timedelta_format(timedelta(days=365 * 3)) == "3 years"
 
 
-def test_td_format_days():
-    assert td_format(timedelta(days=10)) == "10 days"
+def test_timedelta_format_months():
+    assert timedelta_format(timedelta(days=30 * 6)) == "6 months"
 
 
-def test_td_format_hours():
-    assert td_format(timedelta(hours=12)) == "12 hours"
+def test_timedelta_format_days():
+    assert timedelta_format(timedelta(days=10)) == "10 days"
 
 
-def test_td_format_minutes():
-    assert td_format(timedelta(minutes=30)) == "30 minutes"
+def test_timedelta_format_hours():
+    assert timedelta_format(timedelta(hours=12)) == "12 hours"
 
 
-def test_td_format_seconds():
-    assert td_format(timedelta(seconds=45)) == "45 seconds"
+def test_timedelta_format_minutes():
+    assert timedelta_format(timedelta(minutes=30)) == "30 minutes"
+
+
+def test_timedelta_format_seconds():
+    assert timedelta_format(timedelta(seconds=45)) == "45 seconds"
 
 
 @pytest.mark.parametrize(
     "td_input", [timedelta(days=1), timedelta(hours=1), timedelta(minutes=1), timedelta(seconds=1)]
 )
-def test_td_format_positive_input(td_input):
-    output = td_format(td_input)
+def test_timedelta_format_positive_input(td_input):
+    output = timedelta_format(td_input)
     assert int(output.split(" ")[0]) == 1
 
 
 @pytest.mark.parametrize(
     "td_input", [timedelta(days=0), timedelta(hours=0), timedelta(minutes=0), timedelta(seconds=0)]
 )
-def test_td_format_zero_input(td_input):
-    assert td_format(td_input) == ""
+def test_timedelta_format_zero_input(td_input):
+    assert timedelta_format(td_input) == ""
 
 
 @pytest.mark.parametrize(
     "td_input", [timedelta(days=-1), timedelta(hours=-1), timedelta(minutes=-1), timedelta(seconds=-1)]
 )
-def test_td_format_negative_input(td_input):
-    assert td_format(td_input) == ""
+def test_timedelta_format_negative_input(td_input):
+    assert timedelta_format(td_input) == ""
 
 
-def test_td_format_invalid_input():
+def test_timedelta_format_invalid_input():
     class InvalidInput:
         pass
 
     invalid_input = InvalidInput()
     with pytest.raises(AttributeError):
-        td_format(invalid_input)
+        timedelta_format(invalid_input)
