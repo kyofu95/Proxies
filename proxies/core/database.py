5cbde3e8662ea40f9b20b928ef9ab42865ed80e6
sny <127958265+kyofu95@users.noreply.github.com>
2023-06-12 19:30:38 +0300

    db: check db connection

--- proxies/core/database.py
+++ proxies/core/database.py
@@ -1,6 +1,9 @@
+import logging
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from sqlalchemy.exc import SQLAlchemyError
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -11,3 +14,11 @@ def init_database(app: Flask):
 
     db.init_app(app)
     migrate.init_app(app, db)
+
+    with app.app_context():
+        with db.engine.begin() as conn:
+            try:
+                result = conn.exec_driver_sql("select 1;")
+                result.one()
+            except SQLAlchemyError as exc:
+                logging.exception("Database is not connected", exc_info=exc)
