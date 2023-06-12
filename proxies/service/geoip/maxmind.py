c82cc6a5b779e00d350335fd60f956cc1dfce60a
sny <127958265+kyofu95@users.noreply.github.com>
2023-06-12 19:28:51 +0300

    geoip: handle reader exceptions

--- proxies/service/geoip/maxmind.py
+++ proxies/service/geoip/maxmind.py
@@ -1,3 +1,5 @@
+import logging
+
 import geoip2.database
 
 from proxies.service.address import Address
@@ -19,15 +21,25 @@ class MaxmindLiteDb2Geolocation(IBaseGeolocation):
     # The path to the GeoLite2-City.mmdb file.
     litedb2_path = settings.MAXDBLITE_PATH
 
+    def __init__(self) -> None:
+        try:
+            self.db_reader = geoip2.database.Reader(self.litedb2_path)
+        except FileNotFoundError as exc:
+            logging.exception("litedb2 was not found with path %s", self.litedb2_path, exc_info=exc)
+            raise exc
+        except Exception as exc:
+            logging.exception("geoip2 reader failed", exc_info=exc)
+            raise exc
+
+
     def get_address(self, proxy: Proxy) -> Address:
         """Get the geolocation information for the given IP address using the MaxMind GeoLite2 City database."""
 
-        with geoip2.database.Reader(self.litedb2_path) as reader:
-            try:
-                response = reader.city(proxy.ip_address)
-            except geoip2.errors.AddressNotFoundError:
-                return Address()
+        try:
+            response = self.db_reader.city(proxy.ip_address)
+        except geoip2.errors.AddressNotFoundError:
+            return Address()
 
-            return Address(
-                city=response.city.name, region=response.subdivisions.most_specific.name, country=response.country.name
-            )
+        return Address(
+            city=response.city.name, region=response.subdivisions.most_specific.name, country=response.country.name
+        )
