d8190c3694b3a34e0bffd26b7b9f10ecee59c8ef
sny <127958265+kyofu95@users.noreply.github.com>
2023-06-12 19:31:34 +0300

    rework config variables for docker

--- proxies/core/config.py
+++ proxies/core/config.py
@@ -5,9 +5,13 @@ class Settings:
     """This class represents the settings for the application."""
 
     ENVIRONMENT: str = os.environ.get("ENVIRONMENT", "dev")
-    DATABASE_URI: str = os.environ.get("DATABASE_URI", "")
+    POSTGRES_USER: str = os.environ.get("POSTGRES_USER", "")
+    POSTGRES_PASSWORD: str = os.environ.get("POSTGRES_PASSWORD", "")
+    POSTGRES_HOST: str = os.environ.get("POSTGRES_HOST", "")
+    POSTGRES_DB: str = os.environ.get("POSTGRES_DB", "")
+    DATABASE_URI: str = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:5432/{POSTGRES_DB}"
     SECRET_KEY: str = os.environ.get("SECRET_KEY", "")
-    MAXDBLITE_PATH: str = os.environ.get("MAXDBLITE_PATH", "")
+    MAXDBLITE_PATH: str = os.environ.get("MAXDBLITE_PATH", "/opt/GeoLite2-City.mmdb")
 
 
 settings = Settings()
