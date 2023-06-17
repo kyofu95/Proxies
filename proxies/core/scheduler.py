57b8f6b305fc17a2ec813257e8ce5ab53e834363
sny <127958265+kyofu95@users.noreply.github.com>
2023-06-17 17:34:22 +0300

    scheduler: add loging

--- proxies/core/scheduler.py
+++ proxies/core/scheduler.py
@@ -1,8 +1,19 @@
+import logging
+
 from flask import Flask
 
+from apscheduler.events import EVENT_JOB_ERROR
 from apscheduler.schedulers.background import BackgroundScheduler
 
 scheduler = BackgroundScheduler()
+
+
+def listener(event):
+    """APScheduler listener."""
+
+    scheduler.app.logger.exception(
+        "Job %i raised %s", event.job_id, event.exception.__class__.__name__, exc_info=event.exception
+    )
 
 
 def init_scheduler(app: Flask):
@@ -10,3 +21,5 @@ def init_scheduler(app: Flask):
 
     scheduler.app = app
     scheduler.start()
+
+    scheduler.add_listener(listener, EVENT_JOB_ERROR)
